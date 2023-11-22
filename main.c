2f7ed3b76fb0791e5d71ac1dcea3534696c10c25
DANILAO1 <danilagon@icloud.com>
2023-11-22 18:55:53 +0100

    up

---
+++ main.c
@@ -0,0 +1,45 @@
+/*
+-the source code plasma 0.0.1(alpha).
+-the program is still indevelopment and 
+ does not have commands yet.
+-check the readme file file for more informations.
+
+-copyrighted by plasma devteam and Dandev(DANILAO1)
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+
+    char command[100];
+    bool run = 1;
+
+    printf("\n\n\n#################################################\n");
+    printf("                    -plasma-                     \n");
+    printf("                   =v 0.0.1a=                    \n");
+    printf("#################################################\n\n\n");
+
+    while(run) {
+        printf("user/:>");
+        fgets(command, 100, stdin);
+        command[strlen(command)-1] = '\0';
+
+        if (strcmp(command, "plasma -q") == 0) {
+            run = 0;
+        }
+
+        else if(strcmp(command, "-help")==0) {
+            printf("working commands:\nplasma -q(exits from plasma)\nplasma --v(tells the version of plasma)");
+        }
+        else if (strcmp(command, "plasma --v") == 0) {
+            printf("plasma version\n0.0.1 alpha\n");
+        }
+        else {
+            printf("command \"%s\" is unknown!\n", command);
+        }
+    }
+
+    return 0;
+}
